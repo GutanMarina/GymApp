4745f52bf3a59b316ce55c65ddaabab3251735c4
valeriacucoara <valericacucoara@gmail.com>
2025-05-14 17:09:09 +0300

    Add SessionBL

---
+++ eGym.BusinessLogic/blstruct/SessionBL.cs
@@ -0,0 +1,27 @@
+﻿using eGym.BusinessLogic.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eGym.BusinessLogic.Interfaces;
+using eGym.Domain.Entities.User;
+using eGym.Domain.Model.User;
+using System.Data.Entity;
+using System.Web;
+using eGym.BusinessLogic.DBModel;
+using eGym.Domain.Entities;
+
+
+namespace eGym.BusinessLogic.BlStruct
+{
+    public class SessionBL : UserAPI, ISession
+    {
+        public void UserRegister(UDbTable data)
+        {
+            UserRegisterAction(data);
+        }
+
+    }
+
+}
