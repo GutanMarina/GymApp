81dcbb9826bee243ac42b085485b13a4fc17c254
valeriacucoara <valericacucoara@gmail.com>
2025-05-14 17:07:38 +0300

    Add BussinesLogic

---
+++ eGym.BusinessLogic/BussinesLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using eGym.BusinessLogic.BlStruct;
+using eGym.BusinessLogic.Interfaces;
+using eGym.BusinessLogic.BlStruct;
+using eGym.BusinessLogic.Interfaces;
+
+namespace eGym.BusinessLogic
+{//implimentarea la metoda
+    public class BusinessLogic
+    {
+        public ISession GetAuthBl()
+        {
+            return new LoginBl();
+        }
+
+    }
+}
