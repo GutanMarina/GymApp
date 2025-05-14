31cb5d6d7054f7995856203a633bb3cb7eb75477
valeriacucoara <valericacucoara@gmail.com>
2025-05-14 17:10:57 +0300

    Add interfaces

---
+++ eGym.BusinessLogic/interfaces/ISession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eGym.Domain.Entities.User;
+using eGym.Domain.Model.User;
+
+namespace eGym.BusinessLogic.Interfaces
+{//declaram metoda
+    public interface ISession
+    {
+        void UserRegister(UDbTable data);
+    }
+}
+
+
