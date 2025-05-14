543d5dd0a2b61f5be79a068f24ad1c046d6da478
valeriacucoara <valericacucoara@gmail.com>
2025-05-14 17:12:45 +0300

    Add enums

---
+++ eGym.Domain/enums/URole.cs
@@ -0,0 +1,20 @@
+﻿using eGym.Domain.Model.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eGym.Domain.Enums
+{
+    public enum URole
+    {
+        User = 1,
+        Admin = 2
+
+
+    }
+}
+
+
+
