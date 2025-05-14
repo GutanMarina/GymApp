6fa867642e8ad8b4e81c715ef47dd4cafbcc530a
valeriacucoara <valericacucoara@gmail.com>
2025-05-14 17:09:32 +0300

    Add core

---
+++ eGym.BusinessLogic/core/UserAPI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eGym.BusinessLogic.DBModel;
+using eGym.Domain.Entities.User;
+using eGym.Domain.Model.User;
+using System.Linq;
+using System.Data.Entity;
+namespace eGym.BusinessLogic.Core
+{//implementarea metodei
+    public class UserAPI
+    {
+        public string UserAuthLogicAction(UserLoginDTO data)
+        {
+            return "token key";
+        }
+        public void UserRegisterAction(UDbTable data)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                if (db.UDbTable.Any(u => u.Email == data.Email || u.Username == data.Username))
+                {
+                    throw new Exception("Acest utilizator există deja!");
+                }
+
+                db.UDbTable.Add(data);
+                db.SaveChanges();
+            }
+        }
+    }
+}
+
