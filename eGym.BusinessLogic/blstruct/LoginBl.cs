9f5599e0a2ffc6163d3884ae3730d0ae81be7a64
valeriacucoara <valericacucoara@gmail.com>
2025-05-14 17:08:36 +0300

    Add LoginBl

---
+++ eGym.BusinessLogic/blstruct/LoginBl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eGym.BusinessLogic.Core;
+using eGym.BusinessLogic.Interfaces;
+using eGym.Domain.Entities.User;
+using eGym.Domain.Model.User;
+
+namespace eGym.BusinessLogic.BlStruct
+{//legatura businesslog cu metoda, declararea metodei, implementare din interfata cu parametru in useriapi
+    public class LoginBl : UserAPI, ISession
+    {
+
+        public string UserAuthLogic(UserLoginDTO data)
+        {
+            return UserAuthLogicAction(data);
+        }
+
+
+        public void UserRegister(UDbTable data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
