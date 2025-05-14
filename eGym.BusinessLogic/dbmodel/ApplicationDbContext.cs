544dbd6884b04bf29e3f5576cf294b86921a9601
valeriacucoara <valericacucoara@gmail.com>
2025-05-14 17:10:01 +0300

    Add dbmodel

---
+++ eGym.BusinessLogic/dbmodel/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eGym.Domain.Entities.User;
+using eGym.Domain.Entities;
+
+namespace eGym.BusinessLogic.DBModel
+{
+    class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() :
+            base("name = eGymLife")
+        {
+        }
+        public DbSet<UDbTable> UDbTable { get; set; }
+
+    }
+
+
+}
+
+
